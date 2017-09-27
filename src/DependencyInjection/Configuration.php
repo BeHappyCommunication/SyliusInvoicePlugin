ed66ba79a6642c05e4efd3b1b1f19b7026c105d6
Roshyo <stefdecock68@gmail.com>
2017-09-27 10:57:07 +0200

    Renaming plugin

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Acme\ExamplePlugin\DependencyInjection;
+namespace BHC\InvoicePlugin\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
