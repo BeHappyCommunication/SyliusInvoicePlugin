07358c3b614d2f00406d82ac9e344df602be0b3a
Roshyo <stefdecock68@gmail.com>
2017-09-28 09:35:37 +0200

    Ajout des fonctionnalités du front

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -4,21 +4,38 @@ namespace BHC\InvoicePlugin\Controller;
 
 use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 use Sylius\Component\Core\Model\Order;
+use Sylius\Component\Core\Model\OrderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class InvoiceController extends Controller
 {
-    
-    public function renderPdfAction(Request $request)
+    /**
+     * @param Request $request
+     *
+     * @return PdfResponse
+     */
+    public function renderPdfAction(Request $request): PdfResponse
     {
-        $orderId = (int)$request->get('id');
-        $order = $this->container->get('sylius.repository.order')->find($orderId);
+        if(!empty($request->get('id'))){
+            $orderId = (int)$request->get('id');
+            $order = $this->container->get('sylius.repository.order')->find($orderId);
+        }elseif(!empty($request->get('number'))){
+            $orderNumber = $request->get('number');
+            $order = $this->container->get('sylius.repository.order')->findOneBy(['number' => $orderNumber]);
+        }else{
+            throw new BadRequestHttpException('Missing mandatory parameter');
+        }
         if(!$order instanceof Order)
             throw new NotFoundHttpException('The "order" has not been found');
     
-        $html = $this->renderView('BHCInvoicePlugin:Invoice:pdf.html.twig', [
+        if($order->getState() !== OrderInterface::STATE_FULFILLED)
+            throw new BadRequestHttpException('Order not fulfilled');
+    
+        $html = $this->renderView('AppBundle:Invoice:pdf.html.twig', [
             'order' => $order
         ]);
         
@@ -28,14 +45,29 @@ class InvoiceController extends Controller
         );
     }
     
-    public function renderAction(Request $request)
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function renderAction(Request $request): Response
     {
-        $orderId = (int)$request->get('id');
-        $order = $this->container->get('sylius.repository.order')->find($orderId);
+        if(!empty($request->get('id'))){
+            $orderId = (int)$request->get('id');
+            $order = $this->container->get('sylius.repository.order')->find($orderId);
+        }elseif(!empty($request->get('number'))){
+            $orderNumber = $request->get('number');
+            $order = $this->container->get('sylius.repository.order')->findOneBy(['number' => $orderNumber]);
+        }else{
+            throw new BadRequestHttpException('Missing mandatory parameter');
+        }
         if(!$order instanceof Order)
             throw new NotFoundHttpException('The "order" has not been found');
     
-        return $this->render('BHCInvoicePlugin:Invoice:pdf.html.twig', [
+        if($order->getState() !== OrderInterface::STATE_FULFILLED)
+            throw new BadRequestHttpException('Order not fulfilled');
+    
+        return $this->render('AppBundle:Invoice:pdf.html.twig', [
             'order' => $order
         ]);
     }
